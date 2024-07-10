6fbd7c95e0ff1b8af07c8f51fd683a7cb0e1c63c
SaNjida1-2 <158257999+SaNjida1-2@users.noreply.github.com>
2024-07-10 14:31:51 +0600

    homepage and general connections

---
+++ client/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+          Welcome to Notun Jama, your one-stop destination for premium quality products at unbeatable prices. Founded with a passion for excellence and a commitment to customer satisfaction, we curate a diverse range of items to meet all your needs. From the latest fashion trends and cutting-edge electronics to home essentials and unique gifts, we ensure every product in our catalog meets our high standards of quality and value. Our mission is to provide a seamless and enjoyable shopping experience, backed by fast shipping and exceptional customer service. Join our growing community of satisfied customers and discover why Notun Jama is the preferred choice for online shopping.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
