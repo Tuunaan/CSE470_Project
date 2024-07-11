1860e606f7cc6b107e821b0ad22dd65cfd8058c5
Humayera Tabassum Tunan <136461341+Tuunaan@users.noreply.github.com>
2024-07-11 15:45:08 +0600

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -14,13 +14,6 @@ const About = () => {
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2 montserrat-font">
-            <b>🎨 Customization Services:</b> At Notun Jama, we believe in celebrating individuality. That’s why we offer personalized customization options. Want a dress with specific embroidery or a unique color combination? Our skilled artisans can bring your vision to life. From bridal wear to everyday essentials, we tailor garments that reflect your personality.
-          </p>
-          <p className="text-justify mt-2 montserrat-font">
-          <b>🛒 Seamless Shopping Experience:</b> Navigate our user-friendly app effortlessly. Browse by category, filter by size, color, or style, and add your favorite pieces to the cart. Our secure checkout process ensures a smooth transaction, and our responsive customer support team is always ready to assist.
-          </p>
-          <p className="text-justify mt-2 montserrat-font">
-          <b>👗 Women’s Fashion Collection:</b> Explore our diverse range of clothing, from casual wear to formal attire. Discover flowy maxi dresses, tailored blazers, comfy loungewear, and everything in between. Our team of fashion enthusiasts handpicks each item to ensure quality, comfort, and flair.
           </p>
         </div>
       </div>
