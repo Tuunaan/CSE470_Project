9391cd0c86263ab4343d526771c2fc0982430479
Humayera Tabassum Tunan <humayeratabassum@gmail.com>
2024-09-03 07:54:57 +0600

    Products Displayed on Home Page by Tunan

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Layout from "./../components/Layout/Layout";
 import { useAuth } from "../context/auth";
-
-// <<<<<<< HEAD
-// const About = () => {
-//   return (
-//     <Layout title={"About us - Ecommer app"}>
-//       <div className="row contactus">
-//         <div className="col-md-6">
-//           <img
-//             src="/images/threeladies.jpeg"
-//             alt="contactus"
-//             style={{ width: "68%" }}
-//           />
-//         </div>
-//         <div className="col-md-4">
-//           <p className="text-justify mt-2 montserrat-font">
-//           </p>
-//         </div>
-//       </div>
-// =======
+import axios from "axios";
 const HomePage = () => {
   const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  
+ //get products
+ const getAllProducts = async () => {
+  try{
+    const{data}=await axios.get("/api/v1/product/get-product");
+    setProducts(data.products);
+  }catch(error){
+    console.log(error)
+  }
+ }
+ 
+ 
+useEffect(()=>{
+  getAllProducts();
+},[])
+
   return (
-    <Layout title={"Best offers "}>
-      <h1>HomePage</h1>
-      {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
+    <Layout title={"All products - Best offers "}>
+    <div className="row mt-3">
+      <div className="col-md-3">
+        <h4 className="text-center">Filter by Category</h4>
+      </div>
+      <div className="col-md-9">
+        <h1 className="text-center">All Products</h1>
+        <div className="d-flex flex-wrap">
+        {products?.map((p) => (
+                <div className="card m-2" style={{width: '18rem'}} >
+                <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">{p.name}</h5>
+                        <p className="card-text">{p.description}</p>
+                    </div>
+                </div>
+                ))}
+        </div>
+      </div>
+    </div>
     </Layout>
   );
 };
