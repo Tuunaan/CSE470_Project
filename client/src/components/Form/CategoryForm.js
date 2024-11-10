e5be83faaf478da36402ecaa816d6cdf8535c321
Humayera Tabassum Tunan <humayeratabassum@gmail.com>
2024-11-10 15:12:14 +0600

    Tunan

--- client/src/components/Form/CategoryForm.js
+++ client/src/components/Form/CategoryForm.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
-    <>
+    <>    
+    
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <input
--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -1,10 +1,12 @@
 import React from "react";
+
 import { useSearch } from "../../context/search";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const SearchInput = () => {
   const [values, setValues] = useSearch();
   const navigate = useNavigate();
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
