4c1b8ab8fe327d60cd60e6fbb594d5d9d41a74ce
Humayera Tabassum Tunan <humayeratabassum@gmail.com>
2024-11-17 12:15:20 +0600

    msg

--- client/src/context/cart.js
+++ client/src/context/cart.js
@@ -1,20 +1,24 @@
+//tasrif-add to cart 
+
 import { useState, useContext, createContext,useEffect } from "react";
 
 const CartContext = createContext();
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
    useEffect(() => {
+
     let existingCartItem = localStorage.getItem('cart');
     if (existingCartItem) setCart(JSON.parse(existingCartItem));
    }, []); 
   return (
+
     <CartContext.Provider value={[cart, setCart]}>
       {children}
     </CartContext.Provider>
+
   );
 };
 
-// custom hook
 const useCart = () => useContext(CartContext);
-
+// custom hook
 export { useCart, CartProvider };
