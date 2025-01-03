84fdbdb142b0767c16bd2d28ff9bdf960d9097e1
Humayera Tabassum Tunan <humayeratabassum@gmail.com>
2025-01-03 23:25:42 +0600

    admin form edited by Tunan

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="footer">
       {/* <h2 className="text-center">Shop beyond your imagination</h2> */}
       <p className="text-center mt-3">
-        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/about">About</Link>||<Link to="/contact">Contact</Link>||
         <Link to="/policy">Privacy Policy</Link>
       </p>
     </div>
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -15,9 +15,10 @@ const Policy = () => {
 
         <div className="col-md-4">
         <h1 className="custom-bg p-2 text-white text-center">Our Policy</h1>
-          <p>Our privacy policy</p>
-          <p>Our privacy policy</p>
-          <p>Our privacy policy</p>
+          <p>Our privacy policy:   </p>
+          <p>1) Privacy policy 1</p>
+          <p>2) Privacy policy 2</p>
+          <p>2) Privacy policy 3</p>
         </div>
       </div>
     </Layout>
